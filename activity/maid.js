0b12eafd6d2dabc851bf23d6ffe216be4dea80b4
StephDC <admin@hanfubk.com>
2017-03-17 04:17:31 -0400

    Add maid cafe registration pages

---
+++ activity/maid.js
@@ -0,0 +1,80 @@
+var sid = 'None';
+var cf = false;
+function queryBG(parseData){
+	$.ajax({
+		url:"https://worm.hanfucw.com/cgi/maidReservation.py",
+		method:"POST",
+		data:parseData,
+		dataType: "script",
+	});
+}
+
+function searchSpot(){
+	if (!(cf && confirm("Our record suggests you already have a confirmed reservation with Reservation ID of "+sid+".\nDo you want to modify that reservation?"))){
+		data = {
+			act:"search",
+			pop:$("#pop").val(),
+			uName:$("#uName").val(),
+			pNum:$("#pNum").val(),
+			eMail:$("#eMail").val(),
+			aTime:$("#aHour").val()+":"+$("#aMin").val(),
+			rNum:"None",
+		};
+	} else {
+		data = {
+			act:"modify",
+			pop:$("#pop").val(),
+			uName:$("#uName").val(),
+			pNum:$("#pNum").val(),
+			eMail:$("#eMail").val(),
+			aTime:$("#aHour").val()+":"+$("#aMin").val(),
+			rNum:sid,
+		};
+	}
+	queryBG(data);
+}
+
+function lookupSpot(){
+	data = {
+		act:"retrieve",
+		uName:$('#rName').val(),
+		rNum:$('#rNum').val()
+	};
+	queryBG(data);
+}
+
+function cancelSpot(){
+	data = {
+		act:"book",
+		uName:$("#rName").val(),
+		rNum:sid,
+		tNum:"CC"
+	};
+	if (confirm("Are you sure that you want to cancel the reservation with Reservation ID of "+sid+" ?")) queryBG(data);
+}
+
+function getCookie(c_name) {
+        if (document.cookie.length>0){
+                c_start=document.cookie.indexOf(c_name + "=");
+                if (c_start!=-1){
+                        c_start=c_start + c_name.length+1;
+                        c_end=document.cookie.indexOf(";",c_start);
+                        if (c_end==-1) c_end=document.cookie.length;
+                        return unescape(document.cookie.substring(c_start,c_end));
+                }
+        }
+        return "";
+}
+
+
+function setCookie(c_name,c_value) {
+        var exDate = new Date();
+        exDate.setDate(exDate.getDate()+1);
+        document.cookie=/*(document.cookie.length>0?document.cookie + "; ":"")+*/c_name+"="+escape(c_value)+";expires="+exDate.toGMTString()+";path=/";
+}
+
+function remCookie(c_name) {
+        var exDate = new Date();
+        exDate.setDate(exDate.getDate()-1);
+        document.cookie=/*(document.cookie.length>0?document.cookie + "; ":"")+*/c_name+"="+";expires="+exDate.toGMTString()+";path=/";
+}
--- skin/js/main.js
+++ skin/js/main.js
@@ -2,8 +2,7 @@ var pstTimer = -10;
 var porsTimer = -10;
 $('document').ready(function(){
 	/* Starting to define Login link */
-	if ($('#login a').text == 'Login')
-	$('#login a')[0].setAttribute('href','https://bbs.psucssa.org/cgi-bin/webAccess/?cback=acg&pn='+document.location['pathname']);
+	if ($('#login a').text == 'Login') $('#login a')[0].setAttribute('href','https://bbs.psucssa.org/cgi-bin/webAccess/?cback=acg&pn='+document.location['pathname']);
 	/* Starting to define Search bar script */
 	$('#search').mouseout(function(){
 		$('#sform input:first').attr('disabled','');
@@ -47,9 +46,8 @@ $('document').ready(function(){
 	
 });
 function loginTest(args){
-	if (args['name']!= ''){
+	if (args['name']!= '')
 	$("#login a").text(args['name']);
-	}
 }
 var pstId = 0;
 function pstNext(){
